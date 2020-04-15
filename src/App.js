2b7c768fbc9656bc947ea66ad383caa4d30e74ec
lunevaes <luneveks@gmail.com>
2020-04-15 15:30:20 +0300

    Выносит компоненты в отдельные файлы

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
+import Body from './components/Body'
 
 function App() {
   return (
@@ -10,43 +11,5 @@ function App() {
     </div>
   );
 }
-
-const Header = () => {
-  return (
-    <header className="header">
-      <Navigation />
-    </header>
-  )
-}
-
-const Navigation = () => {
-  return (
-    <div className="navigation">
-      <ul className="navigation__list list">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Your Profile</a>
-        </li>
-        <li>
-          <a href="#">Messages</a>
-        </li>
-        <li>
-          <a href="#">Sign Out</a>
-        </li>
-      </ul>
-    </div>
-  )
-}
-
-const Body = () => {
-  return (
-    <main className="App-body">
-      <img src={logo} className="App-logo" alt="logo" />
-    </main>
-  )
-}
-
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <ul className="navigation__list list">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Your Profile</a>
+        </li>
+        <li>
+          <a href="#">Messages</a>
+        </li>
+        <li>
+          <a href="#">Sign Out</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navigation
