45f05d31f0ee38f5845ec75ca32b5a05ef430d2a
lunevaes <luneveks@gmail.com>
2020-04-19 17:46:25 +0300

    Изменяет ссылки в навигации на NavLink

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import {BrowserRouter, Route} from 'react-router-dom'
 import React from 'react'
 
-import Body from './containers/Body/Body'
 import Footer from './containers/Footer/Footer';
 import Header from './containers/Header/Header'
+import Messages from './components/Messages/Messages';
+import Posts from './components/Posts/Posts';
+import Profile from './components/Profile/Profile';
 import classes from './App.module.css'
 
 function App() {
   return (
     <div className={classes.App}>
-      <Header />
-      <Body />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <div className={classes.Body}>
+          <div className={classes.Wrapper}>
+            <Route path='/profile' component={Profile} />
+            <Route path='/messages' component={Messages} />
+            <Route path='/posts' component={Posts} />
+          </div>          
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/containers/Header/Navigation/Navigation.js
+++ src/containers/Header/Navigation/Navigation.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import classes from './Navigation.module.css'
+import {NavLink} from 'react-router-dom'
 
 const Navigation = () => {
   return (
     <div className={classes.navigation}>
       <ul className={classes.list}>
         <li className={classes.item}>
-          <a className={classes.link} href="#">Home</a>
+          <NavLink className={classes.link} to="/">Home</NavLink>
         </li>
         <li className={classes.item}>
-          <a className={classes.link} href="/profile">Your Profile</a>
+          <NavLink className={classes.link} activeClassName={classes.active} to="/profile">Your Profile</NavLink>
         </li>
         <li className={classes.item}>
-          <a className={classes.link} href="/messages">Messages</a>
+          <NavLink className={classes.link} activeClassName={classes.active} to="/messages">Messages</NavLink>
         </li>
         <li className={classes.item}>
-          <a className={classes.link} href="/posts">Posts</a>
+          <NavLink className={classes.link} activeClassName={classes.active} to="/posts">Posts</NavLink>
         </li>
         <li className={classes.item}>
-          <a className={classes.link} href="#">Sign Out</a>
+          <NavLink className={classes.link} activeClassName={classes.active} to="/auto">Sign Out</NavLink>
         </li>
       </ul>
     </div>
