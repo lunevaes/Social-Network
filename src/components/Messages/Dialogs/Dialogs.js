9528731b1d8d383778e1a2a3e303c8ac56fbfbfa
lunevaes <luneveks@gmail.com>
2020-05-05 12:25:09 +0300

    Добавляет поля для постов и сообщений

--- src/components/Messages/Dialogs/Dialogs.js
+++ src/components/Messages/Dialogs/Dialogs.js
@@ -2,6 +2,13 @@ import React from 'react'
 import classes from './Dialogs.module.css'
 
 const Dialog = (data, id) => {
+  let newAnswer = React.createRef()
+
+  let addAnswer = () => {
+    let text = newAnswer.current.value
+    alert(text)
+  }
+
   return (<div className={classes.Dialog}>
     <h5 className={classes.Name}>
       {data[id].name}
@@ -9,6 +16,8 @@ const Dialog = (data, id) => {
     <p>
       {data[id].text}
     </p>
+    <textarea className="textarea" ref={newAnswer}></textarea>
+    <button className="button" onClick={addAnswer}>Добавить ответ</button>
   </div>)
 }
 
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -7,7 +7,16 @@ const Posts = (props) => {
     return (<Post topic={post.topic} text={post.text}/>)
   })
 
+  let newPost = React.createRef()
+
+  let addPost = () => {
+    let text = newPost.current.value
+    alert(text)
+  }
+
   return (<div className={classes.Posts}>
+    <textarea className="textarea" ref={newPost}></textarea>
+    <button className="button" onClick={addPost}>Добавить пост</button>
     {posts}
   </div>)
 }
