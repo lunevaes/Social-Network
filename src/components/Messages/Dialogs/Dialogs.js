4b495cd432c088d623cf288c345e6904453ad07b
lunevaes <luneveks@gmail.com>
2020-05-11 23:21:50 +0300

    Выделяет диалог в отдельную компоненту

--- src/components/Messages/Dialogs/Dialogs.js
+++ src/components/Messages/Dialogs/Dialogs.js
@@ -1,25 +1,6 @@
 import React from 'react'
 import classes from './Dialogs.module.css'
-
-const Dialog = (data, id) => {
-  let newAnswer = React.createRef()
-
-  let addAnswer = () => {
-    let text = newAnswer.current.value
-    alert(text)
-  }
-
-  return (<div className={classes.Dialog}>
-    <h5 className={classes.Name}>
-      {data[id].name}
-    </h5>
-    <p>
-      {data[id].text}
-    </p>
-    <textarea className="textarea" ref={newAnswer}></textarea>
-    <button className="button" onClick={addAnswer}>Добавить ответ</button>
-  </div>)
-}
+import Dialog from './Dialog/Dialog'
 
 const Dialogs = (props) => {
 
